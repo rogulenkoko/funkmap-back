d0dbd2bee0c3c8cdec6ecfb400d22b14a2b8683f
TimofeyMilchakov <timofey.milchakov@mail.ru>
2017-06-14 00:12:59 +0300

    добавлен модуль shop, создан класс сущности для магазинов

---
+++ Shop/Funkmap.Shop.Date/Abstract/IShopRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Funkmap.Common.Data.Abstract;
+using Funkmap.Shop.Data.Entities;
+
+namespace Funkmap.Shop.Date.Abstract
+{
+    public interface IShopRepository : IRepository<ShopEntity>
+    {
+                    
+    }
+}
---
+++ Shop/Funkmap.Shop.Date/Entities/ShopEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Funkmap.Common.Data.Abstract;
+
+namespace Funkmap.Shop.Data.Entities
+{
+    public class ShopEntity : Entity
+    {
+        public String StoreName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public String URL { get; set; }
+    }
+}
