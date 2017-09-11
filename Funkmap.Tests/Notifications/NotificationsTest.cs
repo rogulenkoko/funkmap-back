02023d0d6dc8cd195f7e71243b58296213a6a9e1
K.rogulenko <rogulenkoko@gmail.com>
2017-09-12 02:23:26 +0300

    Абстракция над сервисовм уведомлений

--- Funkmap.Tests/Notifications/NotificationsTest.cs
+++ Funkmap.Tests/Notifications/NotificationsTest.cs
@@ -1,9 +1,13 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using Autofac;
 using Funkmap.Auth.Data;
 using Funkmap.Auth.Data.Entities;
 using Funkmap.Common.Modules;
+using Funkmap.Common.RedisMq;
 using Funkmap.Messenger.Services;
 using Funkmap.Module.Auth;
 using Funkmap.Module.Auth.Services;
@@ -29,9 +33,13 @@ namespace Funkmap.Tests.Notifications
             var builder = new ContainerBuilder();
             var redisMqModule = new RedisMqModule();
             redisMqModule.Register(builder);
-            builder.RegisterType<BandInviteNotificationsService>();
-            builder.RegisterType<FunkmapNotificationService>();
+
+            var serviceType = typeof(Service<,>).MakeGenericType(new[] {typeof(Request), typeof(Response)});
+            builder.RegisterType(serviceType).As<IService>();
+
             _container = builder.Build();
+
+           
 
         }
 
@@ -52,6 +60,66 @@ namespace Funkmap.Tests.Notifications
             specificNotificationService.InviteMusicianToGroup(request);
             Thread.Sleep(5000);
             baseNotificationService.PublishBackRequest(new InviteToGroupBackRequest() {RequestId = request.Id, Answer = true});
+        }
+
+
+        [TestMethod]
+        public void AutofacMagic()
+        {
+            var col = _container.Resolve<IEnumerable<IService>>();
+            col.FirstOrDefault().PublishBack(new Response());
+            Thread.Sleep(5000);
+        }
+
+        public interface IService
+        {
+            void PublishBack(IResponse response);
+        }
+
+        public class Service<TRequest, TResponse> : RedisMqProducer, IRedisMqConsumer, IService where  TRequest : class, IRequest
+                                                                                                where TResponse : class , IResponse
+        {
+            private readonly IMessageService _messageService;
+            public Service(IMessageFactory redisMqFactory, IMessageService messageService) : base(redisMqFactory)
+            {
+                _messageService = messageService;
+            }
+
+            public void InitHandlers()
+            {
+                _messageService.RegisterHandler<TRequest>(req=> Publish(req?.GetBody()));
+            }
+
+            private bool Publish(TRequest request)
+            {
+                return true;
+            }
+
+            public void PublishBack(IResponse response)
+            {
+                Publish<TResponse>(response as TResponse);
+            }
+        }
+
+
+        public interface IRequest
+        {
+            string Property { get; }   
+        }
+
+        public class Request : IRequest
+        {
+            public string Property => "test";
+        }
+
+        public interface IResponse
+        {
+            string ResponseProperty { get; }
+        }
+
+        public class Response : IResponse
+        {
+            public string ResponseProperty => "testresponse";
         }
     }
 }
